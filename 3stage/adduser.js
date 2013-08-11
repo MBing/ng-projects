ad7d6a261f278c3c72249bb841c6649f28b6d59f
MBing <bing.martin.photography@gmail.com>
2013-08-11 23:45:06 +0200

    adding signup document from page 46

---
+++ 3stage/adduser.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function AddUserController($scope) {
+	$scope.message = '';
+
+	$scope.addUser = function() {
+		// TODO for the reader: actually save user to database...
+		$scope.message = 'Thanks, ' + $scope.user.first + ', we added you!';
+	};
+}
